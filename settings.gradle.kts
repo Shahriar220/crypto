2d74701cdd39ce03939337762981a1a6b4bc145b
Shahriar220 <shahriar.uchchash@gmail.com>
2025-03-27 21:48:32 +0600

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") } // 🔹 Added JitPack here
+    }
+}
+
+rootProject.name = "Reown"
+include(":app")
